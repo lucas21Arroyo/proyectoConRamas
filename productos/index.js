b3bd3081cc79d04468a62ce2a4b5f52d15d8be25
Lucas Arroyo <lucas.aa68@gmail.com>
2023-05-31 15:17:28 -0300

    agrega funcionalidad agregar

--- productos/index.js
+++ productos/index.js
@@ -1,11 +1,11 @@
-//const {leerJSON,escribirJSON} = require('../data');
-//const Producto = require('./Producto');
+const {leerJSON,escribirJSON} = require('../data');
+const Producto = require('./Producto');
 
 module.exports = {
-    /* productos : leerJSON(),
+    productos : leerJSON(),
     listar : require('./listar'),
     agregar : require('./agregar'),
-    filtrar : require('./filtrar'),
+    /* filtrar : require('./filtrar'),
     editar : require('./editar'),
-    buscar : require('./buscar') */
+    buscar : require('./buscar') */ 
   }; 
